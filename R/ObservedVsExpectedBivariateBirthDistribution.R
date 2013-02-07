6eaa28a3d94b4c9439ada25fd59666b8babe1de5
triffe <tim.riffe@gmail.com>
2013-02-07 18:26:05 +0100

    progress, 40% done. das gupta ref added (decomp)

--- R/ObservedVsExpectedBivariateBirthDistribution.R
+++ R/ObservedVsExpectedBivariateBirthDistribution.R
@@ -23,15 +23,32 @@ ages <- 10:65
 
 #outer(10^c(0:-10))
 
-logBxy <- MinfNA(log(Bxy))
-logExy <- MinfNA(log(expected))
+logBxy      <- MinfNA(log(Bxy))
+logExy      <- MinfNA(log(expected))
 
-ticks <- c(.00001, .00002, .00005, .0001, .0002, .0005, 0.001, .002, .005, .01,.02, .05, .1)
-brks  <- seq(-11.5, -2.5, by = .05)
+TickMaj     <- rev(c(10^(1:-6)))
+TickMajLab  <- c(".000001",".00001",".0001",".001",".01",".1","1","10")
+TickMin     <- c(t(outer(TickMaj, 2:9)))
 
-image.plot(x = ages + .5, y = ages + .5, t(log(Bxy)), 
+
+
+g.xy <- seq(10, 65, by = 5)
+gb.xy <- seq(10, 65, by = 10)
+
+graphics.off()
+dev.new(height = 4, width = 6.5)
+par(mfrow=c(1,2), mar = c(1,1,1,1))
+image(x = ages + .5, y = ages + .5, t(logBxy), 
         xlim = c(10, 66), ylim = c(10, 66), zlim = c(0, 11), 
-        col = colramp(50), axis.args = list(at = log(ticks), labels = ticks))
+        col = colramp(50), axes = FALSE, asp = 1, 
+        panel.first = list(rect(10, 10, 66, 66, col = "#EEEEEE", xpd = TRUE, border = NA), 
+                           segments(g.xy, 10, g.xy, 66, col = "white", lwd = .5),
+                           segments(10, g.xy, 66, g.xy, col = "white", lwd = .5),
+                           text(10, g.xy, g.xy, pos = 2, cex = .5, xpd = TRUE),
+                           text(g.xy, 10, g.xy, pos = 1, cex = .5, xpd = TRUE)))
+leg.y <- seq(0,11,length = 20) * (56 / 11) + 10
+rect(10,leg.y[1:(length(leg.y) - 1)] )
+
 
 
 
