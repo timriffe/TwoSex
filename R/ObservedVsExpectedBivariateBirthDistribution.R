99e7394424c28784c14dd5353f7461e5d73a906c
triffe <tim.riffe@gmail.com>
2013-02-08 11:41:47 +0100

    .

--- R/ObservedVsExpectedBivariateBirthDistribution.R
+++ R/ObservedVsExpectedBivariateBirthDistribution.R
@@ -37,18 +37,26 @@ gb.xy <- seq(10, 65, by = 10)
 
 graphics.off()
 dev.new(height = 4, width = 6.5)
-par(mfrow=c(1,2), mar = c(1,1,1,1))
-image(x = ages + .5, y = ages + .5, t(logBxy), 
+
+brks <- seq(min(logBxy, na.rm = TRUE),max(logBxy, na.rm = TRUE), length.out = 51)
+
+par(mfrow=c(1,2), omi = c(.5,.1,.5,.9))
+image(x = ages + .5, y = ages + .5, logBxy, 
         xlim = c(10, 66), ylim = c(10, 66), zlim = c(0, 11), 
-        col = colramp(50), axes = FALSE, asp = 1, 
+        col = colramp(50), breaks = brks, axes = FALSE, asp = 1, 
         panel.first = list(rect(10, 10, 66, 66, col = "#EEEEEE", xpd = TRUE, border = NA), 
                            segments(g.xy, 10, g.xy, 66, col = "white", lwd = .5),
                            segments(10, g.xy, 66, g.xy, col = "white", lwd = .5),
                            text(10, g.xy, g.xy, pos = 2, cex = .5, xpd = TRUE),
                            text(g.xy, 10, g.xy, pos = 1, cex = .5, xpd = TRUE)))
-leg.y <- seq(0,11,length = 20) * (56 / 11) + 10
-rect(10,leg.y[1:(length(leg.y) - 1)] )
+  levs <- c(1,10,100,1000,10000)         
 
+contour(x = ages + .5, y = ages + .5, logBxy, levels = log(levs), add = TRUE)
+?contour
+leg.y <- seq(0,11,length = 20) * (56 / 11) + 10
+rect(68,leg.y[1:(length(leg.y) - 1)],72, leg.y[2:length(leg.y)],
+        col = colramp(20), border = "#50505050", xpd = TRUE)
+segments(10,10,66,66,col = "#50505050")
 
 
 
