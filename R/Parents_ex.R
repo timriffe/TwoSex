f753c93fb2c0d41d135ae30cbb98f1529940a76f
triffe <tim.riffe@gmail.com>
2013-02-20 19:13:52 +0100

    .

--- R/Parents_ex.R
+++ R/Parents_ex.R
@@ -1,5 +1,3 @@
-
-
 # ----------------------------------------------
 source("/home/triffe/git/DISS/R/UtilityFunctions.R")
 source("/home/triffe/git/DISS/R/MeanFunctions.R")
@@ -120,58 +118,93 @@ legend(70,.067, lty = c(1,1,5,5), col = gray(c(.2,.5,.2,.5)), lwd = c(2,2.5,2,2.
         legend = c("US males", "US females", "ES males", "ES females"), xpd = TRUE)
 dev.off()
 
-#################################################33
+#################################################
+# once again with females 13-50 and males 15-65:
+ages <- 0:110
+ExUSflim <- with(ExUS,Female[Year == 2009])
+ExUSmlim <- with(ExUS, Male[Year == 2009])
+ExESflim <- with(ExES, Female[Year == 2009])
+ExESmlim <- with(ExES, Male[Year == 2009])
+
+ExUSflim[ages < 13 | ages > 50] <- 0
+ExUSmlim[ages < 15 | ages > 65] <- 0
+ExESflim[ages < 13 | ages > 50] <- 0
+ExESmlim[ages < 15 | ages > 65] <- 0
+
+Mex09USlim <- rowSums(ExpectedDx(Px = ExUSmlim, dx = dxmUS[,"2009"]))
+Fex09USlim <- rowSums(ExpectedDx(Px = ExUSflim, dx = dxfUS[,"2009"]))
+Mex09ESlim <- rowSums(ExpectedDx(Px = ExESmlim, dx = dxmES[,"2009"]))
+Fex09ESlim <- rowSums(ExpectedDx(Px = ExESflim, dx = dxfES[,"2009"]))
+
+
+Fxex09USm <- Bex09USm / Mex09USlim
+Fxex09USf <- Bex09USf / Fex09USlim
+Fxex09ESm <- Bex09ESm / Mex09ESlim
+Fxex09ESf <- Bex09ESf / Fex09ESlim
+
+pdf("/home/triffe/git/DISS/latex/Figures/eSFR2009limits.pdf", height = 5, width = 5)
+par(mai = c(.5, .5, .5, .3), xaxs = "i", yaxs = "i")
+plot(x, Fxex09USm, type = 'l', ylim = c(0, .09), xlim = c(0,111), axes = FALSE,
+        col = gray(.2), lwd = 2, xlab = "", ylab = "",
+        panel.first = list(rect(0,0,111,0.09,col = gray(.95), border=NA),
+                abline(h = seq(0,.09,by = .01), col = "white"),
+                abline(v = seq(0, 110, by = 10), col = "white"),
+                text(seq(0, 110, by = 10), 0,seq(0, 110, by = 10), pos = 1, cex = .8, xpd = TRUE),
+                text(0,seq(0, .09, by = .01), seq(0, .09, by = .01), pos = 2, cex = .8, xpd = TRUE),
+                text(55, -.004, expression(e[x]), cex = 1, pos = 1, xpd = TRUE),
+                text(-15,.096, "Fertility Rate", cex = 1, xpd = TRUE, pos = 4)))
+lines(x, Fxex09USf, lwd = 2.5, col = gray(.5))
+lines(x, Fxex09ESm, lwd = 2, col = gray(.2), lty = 5)
+lines(x, Fxex09ESf, lwd = 2.5, col = gray(.5), lty = 5)
+
+legend(70,.087, lty = c(1,1,5,5), col = gray(c(.2,.5,.2,.5)), lwd = c(2,2.5,2,2.5),bty = "n",
+        legend = c("US males", "US females", "ES males", "ES females"), xpd = TRUE)
+dev.off()
+
+#################################################
 # Surface test:
 
 exSFRUSm <- do.call(cbind, lapply(as.character(yearsUS), function(yr, .BxUS, .ExUS, .dxmUS){
-            Bex <- rowSums(ExpectedDx(Px = rowSums(Mna0(.BxUS[["2009"]])), dx = .dxmUS[,"2009"]))
-            Eex <- rowSums(ExpectedDx(Px = with(.ExUS, Male[Year == as.integer(yr)]), dx = .dxmUS[,"1975"]))
+            Bex <- rowSums(ExpectedDx(Px = rowSums(Mna0(.BxUS[[yr]])), dx = .dxmUS[,yr]))
+            Eex <- rowSums(ExpectedDx(Px = with(.ExUS, Male[Year == as.integer(yr)]), dx = .dxmUS[,yr]))
             Bex / Eex
         }, .BxUS = BxUS, .ExUS = ExUS, .dxmUS = dxmUS))
 exSFRUSf <- do.call(cbind, lapply(as.character(yearsUS), function(yr, .BxUS, .ExUS, .dxfUS){
-            Bex <- rowSums(ExpectedDx(Px = colSums(Mna0(.BxUS[["2009"]])), dx = .dxfUS[,"2009"]))
-            Eex <- rowSums(ExpectedDx(Px = with(.ExUS,Female[Year == as.integer(yr)]), dx = .dxfUS[,"1975"]))
+            Bex <- rowSums(ExpectedDx(Px = colSums(Mna0(.BxUS[[yr]])), dx = .dxfUS[,yr]))
+            Eex <- rowSums(ExpectedDx(Px = with(.ExUS,Female[Year == as.integer(yr)]), dx = .dxfUS[,yr]))
             Bex / Eex
         }, .BxUS = BxUS, .ExUS = ExUS, .dxfUS = dxfUS))
 colnames(exSFRUSm) <- colnames(exSFRUSf) <- yearsUS
 
 exSFRESm <- do.call(cbind, lapply(as.character(yearsES), function(yr, .BxES, .ExES, .dxmES){
-                    Bex <- rowSums(ExpectedDx(Px = rowSums(Mna0(.BxES[["2009"]])), dx = .dxmES[,"2009"]))
-                    Eex <- rowSums(ExpectedDx(Px = with(.ExES, Male[Year == as.integer(yr)]), dx = .dxmES[,"1975"]))
+                    Bex <- rowSums(ExpectedDx(Px = rowSums(Mna0(.BxES[[yr]])), dx = .dxmES[,yr]))
+                    Eex <- rowSums(ExpectedDx(Px = with(.ExES, Male[Year == as.integer(yr)]), dx = .dxmES[,yr]))
                     Bex / Eex
                 }, .BxES = BxES, .ExES = ExES, .dxmES = dxmES))
 exSFRESf <- do.call(cbind, lapply(as.character(yearsES), function(yr, .BxES, .ExES, .dxfES){
-                    Bex <- rowSums(ExpectedDx(Px = colSums(Mna0(.BxES[["2009"]])), dx = .dxfES[,"2009"]))
-                    Eex <- rowSums(ExpectedDx(Px = with(.ExES,Female[Year == as.integer(yr)]), dx = .dxfES[,"1975"]))
+                    Bex <- rowSums(ExpectedDx(Px = colSums(Mna0(.BxES[[yr]])), dx = .dxfES[,yr]))
+                    Eex <- rowSums(ExpectedDx(Px = with(.ExES,Female[Year == as.integer(yr)]), dx = .dxfES[,yr]))
                     Bex / Eex
                 }, .BxES = BxES, .ExES = ExES, .dxfES = dxfES))
 colnames(exSFRESm) <- colnames(exSFRESf) <- yearsES
+
 
 colramp <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(9, "YlOrRd"), space = "Lab")
 
 ages <- 10:65
 
-#outer(10^c(0:-10))
-
-lexSFRUSm       <- MinfNA(log(exSFRUSm))
-lexSFRUSf       <- MinfNA(log(exSFRUSf))
-
-TickMaj         <- rev(c(10^(0:4)))
-TickMajLab      <- c("10000","1000","100","10","1")
-TickMin         <- t(outer(TickMaj[2:length(TickMaj)], 2:9))
-
-g.xy            <- seq(0, 10, by = 5)
-gb.xy           <- seq(0, 10, by =10)
-
-
 #dev.new(height = 4, width = 6.5)
 
-brks <- seq(min(exSFRUSm, na.rm = TRUE),max(exSFRUSf, na.rm = TRUE), length.out = 51)
+brks <- seq(0,.1, length.out = 51)
 levs <- seq(0,.12,by = .01)
-par(mfrow=c(1,2), mar = c(2,1,2,3))
+shift <- 50
+
+# for USA:
+pdf("/home/triffe/git/DISS/latex/Figures/eSFRsurfacesUS.pdf", height = 5, width = 5)
+par(mai = c(.3,.3,.3,1))
 # males
 image(x = yearsUS + .5, y = 0:110 + .5, t(exSFRUSm), 
-        xlim = c(1969, 2010), ylim = c(0, 104), zlim = c(0, .12), 
+        xlim = c(1969, 2010 + shift), ylim = c(0, 104), zlim = c(0, .12), 
         col = colramp(50), breaks = brks, axes = FALSE, asp = 1,
         xlab = "", ylab = "")
 abline(v=seq(1970,2010,by=10), col = "#FFFFFF50")
@@ -181,15 +214,136 @@ contour(x = yearsUS + .5, y = 0:110 + .5, t(exSFRUSm),
         levels = levs, labels = levs, add = TRUE)
 
 # females
-image(x = yearsUS + .5, y = 0:110 + .5, t(exSFRUSf), 
-        xlim = c(1969, 2010), ylim = c(0, 104), zlim = c(0, .12), 
+image(x = yearsUS + .5 + shift, y = 0:110 + .5, t(exSFRUSf), 
+        xlim = c(1969, 2010) + shift, ylim = c(0, 104), zlim = c(0, .12), 
+        col = colramp(50), breaks = brks, axes = FALSE, asp = 1,
+        xlab = "", ylab = "", add = TRUE)
+abline(v=seq(1970,2010,by=10) + shift, col = "#FFFFFF50")
+abline(h=seq(0,100,by=10), col = "#FFFFFF50")
+# contours
+levs <- seq(0,.1,by = .01)
+contour(x = yearsUS + .5 + shift, y = 0:110 + .5, t(exSFRUSf),
+        levels = levs, labels = levs, add = TRUE)
+
+# tick labels: 
+text(1969, seq(0, 100, by = 10), seq(0, 100, by = 10), pos = 2, cex = .7, xpd = TRUE)
+text(1969 + shift, seq(0, 100, by = 10), seq(0, 100, by = 10), pos = 2, cex = .7, xpd = TRUE)
+text(seq(1970, 2010, by = 10), 0, seq(1970, 2010, by = 10), pos = 1, cex = .7, xpd = TRUE)
+text(seq(1970, 2010, by = 10) + shift, 0, seq(1970, 2010, by = 10), pos = 1, cex = .7, xpd = TRUE)
+# axis labels
+text(c(1965, 1965 + shift), 108, expression(e[x]), xpd = TRUE)
+text(c(1990, 1990 + shift), -7, "Year", xpd = TRUE)
+text(c(1990, 1990 + shift), 108, c("Male","Female"), cex = .8, xpd = TRUE)
+# legend
+.y  <- seq(0,104, length.out = 21)
+.y2 <- seq(0,104, length.out = 11)
+rect(2064, .y[1:(length(.y)-1)], 2070, .y[2:length(.y)], col = colramp(20), border = NA, xpd = TRUE)
+segments(2070, .y2, 2071, .y2, xpd = TRUE)
+text(2071, .y2, seq(0, .1, length.out = 11), pos = 4, xpd = TRUE, cex = .7)
+dev.off()
+
+# for Spain:
+pdf("/home/triffe/git/DISS/latex/Figures/eSFRsurfacesES.pdf", height = 5, width = 5)
+par(mai = c(.3,.3,.3,1))
+# males
+image(x = yearsES + .5, y = 0:110 + .5, t(exSFRESm), 
+        xlim = c(1969, 2010 + shift), ylim = c(0, 104), zlim = c(0, .12), 
         col = colramp(50), breaks = brks, axes = FALSE, asp = 1,
         xlab = "", ylab = "")
 abline(v=seq(1970,2010,by=10), col = "#FFFFFF50")
 abline(h=seq(0,100,by=10), col = "#FFFFFF50")
 # contours
-levs <- seq(0,.1,by = .01)
-contour(x = yearsUS + .5, y = 0:110 + .5, t(exSFRUSf),
+contour(x = yearsES + .5, y = 0:110 + .5, t(exSFRESm),
         levels = levs, labels = levs, add = TRUE)
+
+# females
+shift <- 50
+image(x = yearsES + .5 + shift, y = 0:110 + .5, t(exSFRESf), 
+        xlim = c(1969, 2010) + shift, ylim = c(0, 104), zlim = c(0, .12), 
+        col = colramp(50), breaks = brks, axes = FALSE, asp = 1,
+        xlab = "", ylab = "", add = TRUE)
+abline(v=seq(1980,2010,by=10) + shift, col = "#FFFFFF50")
+abline(h=seq(0,100,by=10), col = "#FFFFFF50")
+# contours
+levs <- seq(0,.1,by = .01)
+contour(x = yearsES + .5 + shift, y = 0:110 + .5, t(exSFRESf),
+        levels = levs, labels = levs, add = TRUE)
+
+# tick labels: 
+text(1975, seq(0, 100, by = 10), seq(0, 100, by = 10), pos = 2, cex = .7, xpd = TRUE)
+text(1975 + shift, seq(0, 100, by = 10), seq(0, 100, by = 10), pos = 2, cex = .7, xpd = TRUE)
+text(seq(1980, 2010, by = 10), 0, seq(1980, 2010, by = 10), pos = 1, cex = .7, xpd = TRUE)
+text(seq(1980, 2010, by = 10) + shift, 0, seq(1980, 2010, by = 10), pos = 1, cex = .7, xpd = TRUE)
+# axis labels
+text(c(1971, 1971 + shift), 108, expression(e[x]), xpd = TRUE)
+text(c(1990, 1990 + shift), -7, "Year", xpd = TRUE)
+text(c(1990, 1990 + shift), 108, c("Male","Female"), cex = .8, xpd = TRUE)
+# legend
+.y  <- seq(0,104, length.out = 21)
+.y2 <- seq(0,104, length.out = 11)
+rect(2064, .y[1:(length(.y)-1)], 2070, .y[2:length(.y)], col = colramp(20), border = NA, xpd = TRUE)
+segments(2070, .y2, 2071, .y2, xpd = TRUE)
+text(2071, .y2, seq(0, .1, length.out = 11), pos = 4, xpd = TRUE, cex = .7)
+dev.off()
+
+#----------------------------------------------------------------
+# exTFR plot
+
+exTFRmUS <- colSums(exSFRUSm, na.rm = TRUE)
+exTFRfUS <- colSums(exSFRUSf, na.rm = TRUE)
+exTFRmES <- colSums(exSFRESm, na.rm = TRUE)
+exTFRfES <- colSums(exSFRESf, na.rm = TRUE)
+
+pdf("/home/triffe/git/DISS/latex/Figures/exTFR.pdf", height = 5, width = 5)
+par(mai = c(.5, .5, .3, .3), xaxs = "i", yaxs = "i")
+plot(yearsUS, exTFRmUS, type = 'l', ylim = c(1.3, 3), xlim = c(1968,2010), axes = FALSE,
+        col = gray(.2), lwd = 2, xlab = "", ylab = "",
+        panel.first = list(rect(1968, 1.3, 2010, 3,col = gray(.95), border=NA),
+                abline(h = seq(1.3, 3, by = .2), col = "white"),
+                abline(v = seq(1970, 2010, by = 5), col = "white"),
+                text(1968, seq(1.3, 3., by = .2),seq(1.3, 3, by = .2), pos = 2, cex = .8, xpd = TRUE),
+                text(seq(1970, 2010, by = 10), 1.3, seq(1970, 2010, by = 10), pos = 1, cex = .8, xpd = TRUE),
+                text(1990, 1.2, "Year", cex = 1, pos = 1, xpd = TRUE),
+                text(1965, 3.05, expression(e[x], "             -TFR"), cex = 1, xpd = TRUE)))
+lines(yearsUS, exTFRfUS, lwd = 2.5, col = gray(.5))
+lines(yearsES, exTFRmES, lwd = 2, col = gray(.2), lty = 5)
+lines(yearsES, exTFRfES, lwd = 2.5, col = gray(.5), lty = 5)
+
+legend(1995, 3, lty = c(1,1,5,5), col = gray(c(.2,.5,.2,.5)), lwd = c(2,2.5,2,2.5),bty = "n",
+        legend = c("US males", "US females", "ES males", "ES females"), xpd = TRUE)
+dev.off()
+
+# ----------------------------------------------
+# doesn't look good for Spain, but what about removing non-reproductive ages?
+ages <- 0:110
+exSFRUSmlim <- do.call(cbind, lapply(as.character(yearsUS), function(yr, .BxUS, .ExUS, .dxmUS){
+                    Px <- 
+                    Bex <- rowSums(ExpectedDx(Px = rowSums(Mna0(.BxUS[["2009"]])), dx = .dxmUS[,"2009"]))
+                    Eex <- rowSums(ExpectedDx(Px = with(.ExUS, Male[Year == as.integer(yr)]), dx = .dxmUS[,"1975"]))
+                    Bex / Eex
+                }, .BxUS = BxUS, .ExUS = ExUS, .dxmUS = dxmUS))
+exSFRUSflim <- do.call(cbind, lapply(as.character(yearsUS), function(yr, .BxUS, .ExUS, .dxfUS){
+                    Bex <- rowSums(ExpectedDx(Px = colSums(Mna0(.BxUS[["2009"]])), dx = .dxfUS[,"2009"]))
+                    Eex <- rowSums(ExpectedDx(Px = with(.ExUS,Female[Year == as.integer(yr)]), dx = .dxfUS[,"1975"]))
+                    Bex / Eex
+                }, .BxUS = BxUS, .ExUS = ExUS, .dxfUS = dxfUS))
+colnames(exSFRUSmlim) <- colnames(exSFRUSflim) <- yearsUS
+
+exSFRESmlim <- do.call(cbind, lapply(as.character(yearsES), function(yr, .BxES, .ExES, .dxmES){
+                    Bex <- rowSums(ExpectedDx(Px = rowSums(Mna0(.BxES[["2009"]])), dx = .dxmES[,"2009"]))
+                    Eex <- rowSums(ExpectedDx(Px = with(.ExES, Male[Year == as.integer(yr)]), dx = .dxmES[,"1975"]))
+                    Bex / Eex
+                }, .BxES = BxES, .ExES = ExES, .dxmES = dxmES))
+exSFRESflim <- do.call(cbind, lapply(as.character(yearsES), function(yr, .BxES, .ExES, .dxfES){
+                    Bex <- rowSums(ExpectedDx(Px = colSums(Mna0(.BxES[["2009"]])), dx = .dxfES[,"2009"]))
+                    Eex <- rowSums(ExpectedDx(Px = with(.ExES,Female[Year == as.integer(yr)]), dx = .dxfES[,"1975"]))
+                    Bex / Eex
+                }, .BxES = BxES, .ExES = ExES, .dxfES = dxfES))
+colnames(exSFRESmlim) <- colnames(exSFRESflim) <- yearsES
+
+
+
+
+
 
 
