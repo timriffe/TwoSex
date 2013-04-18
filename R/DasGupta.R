5c711c5a69290902e0caea73262db8fb838c7d95
triffe <tim.riffe@gmail.com>
2013-04-17 18:26:57 -0700

    started. progress on code. continue with iterative solution, make time series of r plots. consider other stable parameters if there's time, but maybe not necessary

--- R/DasGupta.R
+++ R/DasGupta.R
@@ -5,71 +5,50 @@ source("/home/triffe/git/DISS/R/MeanFunctions.R")
 # BxUS is a list of 56x56 matrices, ages 10-65, males in rows, females in columns
 # (1969 - 2010)
 # BxES is 0:110, years 1975:2009
-BxUS <- local(get(load("/home/triffe/git/DISS/Data/USbirths/USBxy10_65.Rdata")))
+BxUS <- local(get(load("/home/triffe/git/DISS/Data/USbirths/USBxy0_110.Rdata")))
 BxES <- local(get(load("/home/triffe/git/DISS/Data/ESbirths/ESBxy.Rdata"))) # not cut to 10-65
 # exposures, as such, straiht from HMD, all ages 0-110, long form
 ExUS <- local(get(load("/home/triffe/git/DISS/Data/Exposures/USexp.Rdata")))
 ExES <- local(get(load("/home/triffe/git/DISS/Data/Exposures/ESexp.Rdata")))
+LxmUS <- local(get(load("/home/triffe/git/DISS/Data/HMD_Lx/LxmUS.Rdata"))) / 1e5
+LxfUS <- local(get(load("/home/triffe/git/DISS/Data/HMD_Lx/LxfUS.Rdata"))) / 1e5
+LxmES <- local(get(load("/home/triffe/git/DISS/Data/HMD_Lx/LxmES.Rdata"))) / 1e5
+LxfES <- local(get(load("/home/triffe/git/DISS/Data/HMD_Lx/LxfES.Rdata"))) / 1e5
 
 #--------------------------
 # Das Gupta 1972:
 
-
-
-
-
-
-
-
 # implementing Das Gupta 1978:
+pf. <- LxfUS[,"2000"]
+pm. <- LxmUS[,"2000"]
+Bxy         <- BxUS[["2000"]]
 
+PM <- with(ExUS, Male[Year == 2000])
+PF <- with(ExUS, Female[Year == 2000])        
 
-Bxy         <- BxUS[["1970"]]
-
-Pxm <- with(ExUS, Male[Age >= 10 & Age <= 65 & Year == 1970])
-Pxf <- with(ExUS, Female[Age >= 10 & Age <= 65 & Year == 1970])        
-
-Bxy <- BxUS[["1970"]]
+Bxy <- BxUS[["2000"]]
 # eq 5.4
-Uxy <- Mna0(Bxy %row% rowSums(Bxy))
-Vxy <- Mna0(Bxy %col% colSums(Bxy))
+Uxy <- Minf0(Mna0(Bxy / rowSums(Bxy)))
+Vxy <- Minf0(Mna0(t(t(Bxy) / colSums(Bxy))))
 
-Uxy <- Uxy / sum(Uxy)
-Vxy <- Vxy / sum(Vxy)
+
+# theta is prop male of births
 thetam <- 1.05 / 2.05
 thetaf <- 1 / 2.05
 
-Mxy <- Uxy %row% (Pxm / 1)
-Fxy <- Uxy %col% (Pxf / 1)
 
+mxy <- Minf0(Mna0(Bxy /(Uxy * PM + t(t(Vxy) * PF))))
+thetam * pm. * Uxy 
+thetaf * pf. * t(Vxy)
 
-mxy <- Mna0(Bxy / (Uxy %row% (Pxm / 1) + Vxy %col% (Pxf / 1)))
+Gupta1978Min <- function(r, Uxy, Vxy, lxm, lxf, mxy,.a = .5:110.5){
+    (1 - sum(mxy * (thetam * exp(-r * .a) * pm. * Uxy + t(thetaf * exp(-r * .a) * pf. * t(Vxy)))))^2
+}
+optimize(Gupta1978Min, interval = c(-.02, .02), tol = 1e-15,
+        Uxy = Uxy, Vxy = Vxy, lxm = pm., lxf = pf., mxy = mxy)
+#mxy <- Mna0(Bxy / (Uxy %row% (Pxm / 1) + Vxy %col% (Pxf / 1)))
 
-mx <- rowSums(Bxy) / Pxm
-my <- colSums(Bxy) / Pxf
-mxm  <- thetam * mx
-myf  <- thetaf * my
-mxy <- (thetam / mxm + thetaf / myf) ^ -1
-sum(mxy) 
-sum(mxy)
+unlist(lapply(yearsUS),function())
 
-
-
-plot(10:65, mxy, col = "red", type = "l")
-polygon(c(10:65, 65:10), c(mx, rev(my)), col = "#55555550")
-
-Bt <- sum((thetam * Uxy %row% (Pxm/1) + thetaf * Vxy %col% (Pxf/1)) * )
-
-sum(apply(cbind(mxm / (2 * thetam), myf / (2 * thetaf)), 1, harmonic.mean))
-sum(apply(cbind(mxm / (thetam), myf / ( thetaf)), 1, harmonic.mean))
-sum(apply(cbind(mx, my), 1, harmonic.mean))
-
-(TFRm <- sum(mxm / thetam))
-(TFRf <- sum(myf / thetaf))
-harmonic.mean(c(TFRm, TFRf)) #hmmm
-
-# I've come to the conclusion that Das Gupta essentially banks on a *harmonic mean*, but with no
-# lengthy justification.
-
-
+Gupta1978It <- function()
 
