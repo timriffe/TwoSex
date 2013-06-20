aa1d47c556c6d1bd332e6be02b53d61d4e0e0940
triffe <tim.riffe@gmail.com>
2013-06-20 15:54:29 +0200

    added gap line

--- Pres/R/AgeThanosDecomp.R
+++ Pres/R/AgeThanosDecomp.R
@@ -20,17 +20,16 @@ abline(h = seq(-.004,.007,by=.001), col = "white")
 abline(v = seq(1,41,by=5), col = "white")
 barplot(t(Neg), add = TRUE, space = 0, border = NA, col = cols,width = 1,axes = FALSE)
 barplot(t(Pos), add = TRUE, space = 0, border = NA, col = cols,width = 1,axes = FALSE)
-
+lines(.5:40.5, rowSums(USdecompR), col = "green",lwd=2)
 text(seq(1,41,by=5),-.004,seq(1970,2010,by=5),pos=1,cex=.8,xpd=TRUE)
 text(-1,seq(-.004,.007,by=.001),seq(-.004,.007,by=.001),cex=.8,pos=2,xpd=TRUE)
 text(c(11, 11, 11, 12,32),c(0.000522905,  0.002898587, -0.001163063,  0.005350904,-.0027), 
         c("Fertility shape", "SRB", "Mortality", "eTFR","eTFR"), cex = 1.3,pos = 4, col = c("white","white","white",gray(.2),gray(.2)))
-
 segments(12.604584, 0.005159316, 9.953926, 0.004431285)
 segments(32.97280, -0.002465856, 31.50796, -0.001833618)
-
 text(20,-.005,"Year",xpd=TRUE)
 text(-2,.0075,"Contribution\nto difference in r", pos = 4, xpd = TRUE)
+text(28,.0007,"Gap",col = "green",cex=1.3)
 dev.off()
 
 # Spain
@@ -48,7 +47,7 @@ abline(h = seq(-.004,.007,by=.001), col = "white")
 abline(v = seq(-5,35,by=5), col = "white")
 barplot(t(Neg), add = TRUE, space = 0, border = NA, col = cols, width = 1,axes = FALSE)
 barplot(t(Pos), add = TRUE, space = 0, border = NA, col = cols, width = 1,axes = FALSE)
-
+lines(.5:34.5, rowSums(ESdecompR), col = "green",lwd=2)
 text(seq(-5,35,by=5),-.004,seq(1970,2010,by=5),pos=1,cex=.8,xpd=TRUE)
 text(-7,seq(-.004,.007,by=.001),seq(-.004,.007,by=.001),cex=.8,pos=2,xpd=TRUE)
 text(c(12,12,12,  6, 29),c(0.0009635557,  0.0040289517, -0.0015079198,  0.0058107131, -0.0033), 
@@ -58,8 +57,8 @@ segments(9.185489,0.005580808 ,10.441064,0.005044364)
 segments(32.20436,-0.003002300,32.90191,-0.002389221)
 text(15,-.005,"Year",xpd=TRUE)
 text(-8,.0075,"Contribution\nto difference in r", pos = 4, xpd = TRUE)
+text(18.5,.0025,"Gap",col = "green",cex=1.3)
 dev.off()
-
 
 
 
--- Pres/R/rFigures.R
+++ Pres/R/rFigures.R
@@ -1,4 +1,5 @@
 # figure of r for both sexes and standard Lotka r
+setwd("/home/triffe/git/DISS/")
 
 rLotkaES    <- local(get(load("Data/results/agerSRB/rLotkaES.Rdata")))
 rLotkaUS    <- local(get(load("Data/results/agerSRB/rLotkaUS.Rdata")))
@@ -53,6 +54,8 @@ lines(yearsUS, rmUS[, 1],col = grey(.9), lwd = 2.5)
 lines(yearsUS, rfUS[, 1],col = grey(.9), lwd = 3)
 lines(yearsUS, rLotkaUS[,1], col = Cols[2], lwd = 1)
 lines(yearsUS, rLotkaUS[,2], col = Cols[8], lwd = 1)
+text(c(1974, 1975),c(0.005, -0.008387479),
+        c("US males\nchrono","US females\nchrono"))
 dev.off()
 
 # for age and ey US
@@ -76,6 +79,8 @@ lines(yearsUS, rmUS[, 1],col = Cols[2], lwd = 2.5)
 lines(yearsUS, rfUS[, 1],col = Cols[8], lwd = 3)
 lines(yearsUS, rLotkaUS[,1], col = Cols[2], lwd = 1)
 lines(yearsUS, rLotkaUS[,2], col = Cols[8], lwd = 1)
+text(c(1974, 1975, 1986, 1986),c(0.005, -0.008387479, .004, -.0002),
+        c("US males\nchrono","US females\nchrono","US males\nthano","US females\nthano"))
 dev.off()
 
 pdf("Pres/FiguresStatic/rSingleSex3.pdf", height = 5, width = 5)
@@ -89,7 +94,6 @@ plot(yearsUS, rmUS[, 1], type = 'n', ylim = c(-.02,.011),xlim = c(1968,2010), ax
                 text(seq(1970, 2010, by = 10),-.02, seq(1970, 2010, by = 10), pos = 1, cex = .8, xpd = TRUE),
                 text(1990, -.022, "Year", cex = 1, pos = 1, xpd = TRUE),
                 text(1966,.0125, "r", cex = 1, xpd = TRUE)))
-
 lines(yearsES, rmES[, 1],col = grey(.9), lwd = 2.5)
 lines(yearsES, rfES[, 1],col = grey(.9), lwd = 3)
 lines(yearsUS, rmUS[, 1],col = grey(.9), lwd = 2.5)
@@ -98,6 +102,8 @@ lines(yearsUS, rLotkaUS[,1], col = grey(.9), lwd = 1)
 lines(yearsUS, rLotkaUS[,2], col = grey(.9), lwd = 1)
 lines(yearsES, rLotkaES[,1], col = Cols[2], lwd = 1)
 lines(yearsES, rLotkaES[,2], col =  Cols[8], lwd = 1)
+text(c(1995.5, 1985),c( -0.016, -0.015028590),
+        c("ES males\nchrono","ES females\nchrono"))
 dev.off()
 
 pdf("Pres/FiguresStatic/rSingleSex4.pdf", height = 5, width = 5)
@@ -120,6 +126,8 @@ lines(yearsES, rLotkaES[,1], col = Cols[2], lwd = 1)
 lines(yearsES, rLotkaES[,2], col =  Cols[8], lwd = 1)
 lines(yearsES, rmES[, 1],col = Cols[2], lwd = 2.5)
 lines(yearsES, rfES[, 1],col = Cols[8], lwd = 3)
+text(c(1995.5, 1985,2000,2004.5),c( -0.016, -0.015028590,-0.007145646, -0.010169241),
+        c("ES males\nchrono","ES females\nchrono","ES males\nthano","ES females\nthano"))
 dev.off()
 
 # ----------------------------------------------------------------------------- ## ----------------------------------------------------------------------------- #
